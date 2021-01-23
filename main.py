00b9ce1e2af1e7f3f43b20d9fd959d211d451b72
Michele Conti <11929535+mikcnt@users.noreply.github.com>
2021-01-23 13:22:00 +0100

    Reorganized code: GAN class for training and evaluation

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import torchvision
 from models import Discriminator, Generator, GeneratorLoss
 from dataset import ImageDataset
 from parser import main_parser
-from utils import load_checkpoint, save_checkpoint
+from utils import load_checkpoint, save_checkpoint, DCGAN
 
 
 def main():
@@ -24,7 +24,7 @@ def main():
         "generator": "checkpoints/generator",
     }
 
-    # Create checkpoints directories
+    # Create checkpoints and outputs directories
     os.makedirs(checkpoints_path["discriminator"], exist_ok=True)
     os.makedirs(checkpoints_path["generator"], exist_ok=True)
 
@@ -111,7 +111,7 @@ def main():
         )
         print("Finished loading discriminator checkpoint.")
         print("Resuming training from epoch {}.".format(disc_epoch))
-        
+
     if GENERATOR_CHECKPOINT:
         gen, opt_gen, gen_epoch, gen_loss, test_gen_loss = load_checkpoint(
             gen, opt_gen, GENERATOR_CHECKPOINT
@@ -119,117 +119,31 @@ def main():
         print("Finished loading generator checkpoint.")
         print("Resuming training from epoch {}.".format(gen_epoch))
 
-    saved_images = False
+    gan = DCGAN(disc, gen, opt_disc, opt_gen, criterion, gen_criterion, device)
 
     # Fit GAN
     for epoch in range(gen_epoch + 1, NUM_EPOCHS + 1):
-        epoch_d_loss = 0
-        epoch_g_loss = 0
-        for noise, real in tqdm(
-            train_loader, ncols=70, desc="Epoch {}/{}".format(epoch, NUM_EPOCHS)
-        ):
+        # Train both generator and discriminator
+        train_d_loss, train_g_loss = gan.train_epoch(train_loader, epoch)
+        # Print losses and update dictionaries
+        print(f"Loss D: {train_d_loss:.4f}, loss G: {train_g_loss:.4f}")
 
-            noise = noise.to(device)
-            real = real.to(device)
-            fake = gen(noise)
-
-            # Train Discriminator: max log(D(x)) + log(1 - D(G(z)))
-            disc_real = disc(real).reshape(-1)
-            loss_disc_real = criterion(disc_real, torch.ones_like(disc_real))
-            disc_fake = disc(fake.detach()).reshape(-1)
-            loss_disc_fake = criterion(disc_fake, torch.zeros_like(disc_fake))
-            loss_disc = (loss_disc_real + loss_disc_fake) / 2
-            disc.zero_grad()
-            loss_disc.backward()
-            opt_disc.step()
-
-            # Train Generator: min log(1 - D(G(z))) <-> max log(D(G(z))
-            output = disc(fake).reshape(-1)
-            adv_loss = criterion(output, torch.ones_like(output))
-
-            loss_gen = gen_criterion(adv_loss, fake, real)
-            gen.zero_grad()
-            loss_gen.backward()
-            opt_gen.step()
-
-            epoch_d_loss += loss_disc.item()
-            epoch_g_loss += loss_gen.item()
-
-        # Print losses
-        print(
-            f"Epoch [{epoch}/{NUM_EPOCHS}] Loss D: {epoch_d_loss:.4f}, loss G: {epoch_g_loss:.4f}"
-        )
-
-        # Update losses dictionaries
-        disc_loss[epoch] = epoch_d_loss
-        gen_loss[epoch] = epoch_g_loss
+        disc_loss[epoch] = train_d_loss
+        gen_loss[epoch] = train_g_loss
 
         # Validation
         print("Starting validation for epoch {}.".format(epoch))
-        # Save outputs of the generator each epoch
-        with torch.no_grad():
-            gen_images = []
-            test_d_loss = 0
-            test_g_loss = 0
-            for test_noise, test_real in test_loader:
-                test_real = test_real.to(device)
-                test_noise = test_noise.to(device)
-                test_fake = gen(test_noise)
-                gen_images.append(test_fake)
+        test_d_loss, test_g_loss = gan.evaluate_epoch(test_loader, epoch)
 
-                # Losses for test
-                disc_real = disc(test_real).reshape(-1)
-                loss_disc_real = criterion(disc_real, torch.ones_like(disc_real))
-                disc_fake = disc(test_fake.detach()).reshape(-1)
-                loss_disc_fake = criterion(disc_fake, torch.zeros_like(disc_fake))
-                loss_disc = (loss_disc_real + loss_disc_fake) / 2
-                output = disc(test_fake).reshape(-1)
-                adv_loss = criterion(output, torch.ones_like(output))
-                loss_gen = gen_criterion(adv_loss, test_fake, test_real)
-
-                test_d_loss += loss_disc
-                test_g_loss += loss_gen
-
-            gen_images = torch.cat(gen_images, 0)
-
-            img_grid_fake = torchvision.utils.make_grid(gen_images, nrow=8)
-            torchvision.utils.save_image(
-                img_grid_fake, "outputs/{}_fake.png".format(str(epoch).zfill(3))
-            )
-
-            test_disc_loss[epoch] = test_d_loss
-            test_gen_loss[epoch] = test_g_loss
-
-        # Save real and noisy image during first epoch
-        if not saved_images:
-            noise_images = []
-            real_images = []
-            for test_noise, test_real in test_loader:
-                real_images.append(test_real)
-                noise_images.append(test_noise)
-            noise_images = torch.cat(noise_images, 0)
-            real_images = torch.cat(real_images, 0)
-
-            img_grid_noise = torchvision.utils.make_grid(noise_images, nrow=8)
-            img_grid_real = torchvision.utils.make_grid(real_images, nrow=8)
-
-            torchvision.utils.save_image(img_grid_noise, "outputs/noise.png")
-            torchvision.utils.save_image(img_grid_real, "outputs/real.png")
-
-            saved_images = True
+        test_disc_loss[epoch] = test_d_loss
+        test_gen_loss[epoch] = test_g_loss
 
         # Save checkpoints
+        disc_losses = {"train": disc_loss, "test": test_disc_loss}
 
-        disc_check_path = os.path.join(
-            checkpoints_path["discriminator"],
-            "disc-{}.pth".format(str(epoch).zfill(3)),
-        )
-        gen_check_path = os.path.join(
-            checkpoints_path["generator"], "gen-{}.pth".format(str(epoch).zfill(3))
-        )
+        gen_lossess = {"train": gen_loss, "test": test_gen_loss}
 
-        save_checkpoint(disc, opt_disc, epoch, disc_loss, test_d_loss, disc_check_path)
-        save_checkpoint(gen, opt_gen, epoch, gen_loss, test_g_loss, gen_check_path)
+        gan.save_checkpoints(epoch, disc_losses, gen_lossess)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import os
+from tqdm import tqdm
 import torch
 import torch.nn as nn
+import torchvision
 
 
 def shifted(img):
@@ -41,3 +43,158 @@ def save_checkpoint(model, optimizer, epoch, train_loss, test_loss, path):
     }
 
     torch.save(model_checkpoint, path)
+
+
+class DCGAN(object):
+    def __init__(self, disc, gen, opt_disc, opt_gen, criterion, gen_criterion, device):
+        self.disc = disc
+        self.gen = gen
+        self.opt_disc = opt_disc
+        self.opt_gen = opt_gen
+        self.criterion = criterion
+        self.gen_criterion = gen_criterion
+        self.device = device
+        self.saved_images = False
+
+    def train_disc(self, noise, real):
+        fake = self.gen(noise)
+        disc_real = self.disc(real).reshape(-1)
+        loss_disc_real = self.criterion(disc_real, torch.ones_like(disc_real))
+        disc_fake = self.disc(fake.detach()).reshape(-1)
+        loss_disc_fake = self.criterion(disc_fake, torch.zeros_like(disc_fake))
+        loss_disc = (loss_disc_real + loss_disc_fake) / 2
+        self.disc.zero_grad()
+        loss_disc.backward()
+        self.opt_disc.step()
+
+        return loss_disc
+
+    def train_gen(self, noise, real):
+        fake = self.gen(noise)
+        output = self.disc(fake).reshape(-1)
+        adv_loss = self.criterion(output, torch.ones_like(output))
+        loss_gen = self.gen_criterion(adv_loss, fake, real)
+        self.gen.zero_grad()
+        loss_gen.backward()
+        self.opt_gen.step()
+
+        return loss_gen
+
+    def train_iteration(self, noise, real):
+        loss_disc = self.train_disc(noise, real)
+        loss_gen = self.train_gen(noise, real)
+
+        return loss_disc, loss_gen
+
+    def train_epoch(self, train_loader, epoch):
+        self.gen.train()
+        self.disc.train()
+        epoch_d_loss = 0
+        epoch_g_loss = 0
+        for noise, real in tqdm(train_loader, ncols=70, desc="Epoch {}".format(epoch)):
+            noise = noise.to(self.device)
+            real = real.to(self.device)
+
+            loss_disc, loss_gen = self.train_iteration(noise, real)
+
+            epoch_d_loss += loss_disc.item()
+            epoch_g_loss += loss_gen.item()
+
+        return epoch_d_loss, epoch_g_loss
+
+    def evaluate_iteration(self, noise, real):
+        fake = self.gen(noise)
+
+        disc_real = self.disc(real).reshape(-1)
+        loss_disc_real = self.criterion(disc_real, torch.ones_like(disc_real))
+        disc_fake = self.disc(fake.detach()).reshape(-1)
+        loss_disc_fake = self.criterion(disc_fake, torch.zeros_like(disc_fake))
+        loss_disc = (loss_disc_real + loss_disc_fake) / 2
+        output = self.disc(fake).reshape(-1)
+        adv_loss = self.criterion(output, torch.ones_like(output))
+        loss_gen = self.gen_criterion(adv_loss, fake, real)
+
+        return loss_disc, loss_gen
+
+    # def evaluate_epoch(self, test_loader, epoch):
+    #     self.gen.eval()
+    #     self.disc.eval()
+    #     with torch.no_grad():
+    #         test_d_loss = 0
+    #         test_g_loss = 0
+    #         for test_noise, test_real in test_loader:
+    #             test_real = test_real.to(self.device)
+    #             test_noise = test_noise.to(self.device)
+
+    #             # Losses for test
+    #             loss_disc, loss_gen = self.evaluate_iteration(test_noise, test_real)
+
+    #             test_d_loss += loss_disc.item()
+    #             test_g_loss += loss_gen.item()
+    #     return test_d_loss, test_g_loss
+
+    def evaluate_epoch(self, test_loader, epoch):
+        noise_path = "outputs/noise.png"
+        real_path = "outputs/real.png"
+        gen_path = "outputs/{}_fake.png".format(str(epoch).zfill(3))
+        gen_images = []
+        noise_images = []
+        real_images = []
+
+        self.disc.eval()
+        self.gen.eval()
+        with torch.no_grad():
+            test_d_loss = 0
+            test_g_loss = 0
+            for noise, real in test_loader:
+                noise = noise.to(self.device)
+                real = real.to(self.device)
+                fake = self.gen(noise)
+
+                loss_disc, loss_gen = self.evaluate_iteration(noise, real)
+
+                test_d_loss += loss_disc.item()
+                test_g_loss += loss_gen.item()
+
+                gen_images.append(fake)
+                if not self.saved_images:
+                    noise_images.append(noise)
+                    real_images.append(real)
+
+        gen_images = torch.cat(gen_images, 0)
+        img_grid_fake = torchvision.utils.make_grid(gen_images, nrow=8)
+        torchvision.utils.save_image(img_grid_fake, gen_path)
+
+        if not self.saved_images:
+            noise_images = torch.cat(noise_images, 0)
+            real_images = torch.cat(real_images, 0)
+            img_grid_noise = torchvision.utils.make_grid(noise_images, nrow=8)
+            img_grid_real = torchvision.utils.make_grid(real_images, nrow=8)
+            torchvision.utils.save_image(img_grid_noise, noise_path)
+            torchvision.utils.save_image(img_grid_real, real_path)
+            self.saved_images = True
+
+        return test_d_loss, test_g_loss
+
+    def save_checkpoints(self, epoch, disc_losses, gen_losses):
+        disc_train_loss = disc_losses["train"]
+        disc_test_loss = disc_losses["test"]
+        gen_train_loss = gen_losses["train"]
+        gen_test_loss = gen_losses["test"]
+
+        disc_check_path = "checkpoints/discriminator/disc-{}.pth".format(
+            str(epoch).zfill(3)
+        )
+        gen_check_path = "checkpoints/generator/gen-{}.pth".format(str(epoch).zfill(3))
+
+        save_checkpoint(
+            self.disc,
+            self.opt_disc,
+            epoch,
+            disc_train_loss,
+            disc_test_loss,
+            disc_check_path,
+        )
+        save_checkpoint(
+            self.gen, self.opt_gen, epoch, gen_train_loss, gen_test_loss, gen_check_path
+        )
